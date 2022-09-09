8f3534565ede4086adceb976c3a2920dc674ace0
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-09 20:23:48 +0300

    def

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {DisplayCounter} from './Counter/DisplayCounter/DisplayCounter';
+import UniversalButton from './UniversalButton/UniversalButton';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [counter, setCounter] = useState(0);
+
+
+    const increment = () => setCounter(++counter)
+    const zero = () => setCounter(0)
+
+    const disabledIncButtonOrNot = counter >= 5
+    const disabledResetButtonOrNot = counter === 0
+
+    return (
+        <div className={'main-content'}>
+            <DisplayCounter counter={counter}/>
+            <div className={'block'}>
+                <UniversalButton name={'inc'}
+                                onClick={increment}
+                                disabled={disabledIncButtonOrNot}/>
+                <UniversalButton name={'reset'} onClick={zero}
+                                 disabled={disabledResetButtonOrNot}
+                />
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
