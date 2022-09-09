0b99deb4b5670a14fab5d6561800ad27020b86c7
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-09 18:44:20 +0300

    create

---
+++ src/UniversalButton/UniversalButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UniversalButton = () => {
+    return (
+        <div>
+            <button>name</button>
+        </div>
+    );
+};
+
+export default UniversalButton;
