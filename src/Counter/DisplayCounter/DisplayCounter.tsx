d586c6bd5468e2f3dcf0aef44715a4a32b952068
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-20 00:21:31 +0300

    add checked

--- src/Counter/DisplayCounter/DisplayCounter.tsx
+++ src/Counter/DisplayCounter/DisplayCounter.tsx
@@ -4,15 +4,21 @@ import classes from './DisplayCounter.module.css'
 type DisplayCounter = {
     counter: number
     MAX_VALUE: number
+    incorrectValue: string
+
 }
 
-export const DisplayCounter : React.FC<DisplayCounter> = (props) => {
-    const {counter,MAX_VALUE} = props;
+export const DisplayCounter: React.FC<DisplayCounter> = (props) => {
+    const {counter, MAX_VALUE, incorrectValue} = props;
     const colorCounter = counter === MAX_VALUE ? classes.red : classes.def;
 
+
     return (
-        <div className={classes.displayCounter} >
-            <span className={colorCounter} >{counter}</span>
+        <div className={classes.displayCounter}>
+            {/*{incorrectValue ? incorrectValue :  suc ? suc : <span></span> }*/}
+            {incorrectValue ?
+                <span className={classes.red}>{incorrectValue}</span> :
+                <span className={colorCounter}>{counter}</span>}
         </div>
     );
 };
